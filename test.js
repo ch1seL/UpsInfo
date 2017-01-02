4fd07a43f32995f7e55ab526cc9df5b7dcedb1c8
Александр Саламатов < >
2017-01-02 14:31:23 +0400

    test

--- test.js
+++ test.js
@@ -1,4 +1,19 @@
 var Datastore = require('nedb'),
-    db = new Datastore({ filename: 'db/upshistory.db', autoload: true });
+    db = new Datastore({ filename: 'db/upshistory.db', autoload: true }),
+    db_old = new Datastore({ filename: 'db/upshistory_old.db', autoload: true });
 
-db.update({}, { $set: { dateInMs: new Date("date").getTime() } }, { multi: true })
+db_old.find({ date: { $ne: undefined } }).exec((err, docs) => {
+
+    var qwe = docs.map((item) => {
+
+        date = new Date(item.date);
+        newItem = { date: date.getTime(), epm_temperature: item.epm_temperature, epm_humidity: item.epm_humidity };
+        if (item.mailsend != undefined)
+            newItem.mailsend = item.mailsend;
+
+        return newItem;
+    });
+
+    console.log(qwe);
+     db.insert(qwe);
+})
